f6333c73a72fc1c8bb4d69c375921b22fe08bd6c
PaulO-coder130 <49920895+PaulO-coder130@users.noreply.github.com>
2025-10-29 14:56:44 -0700

    added character animations (just still pictures for now)

--- TheWoodsAreWatching/player.gd
+++ TheWoodsAreWatching/player.gd
@@ -6,6 +6,8 @@ extends CharacterBody2D
 @export var basePointY = 0
 @export var colRadius = 0
 
+@onready var animated_sprite_2D: AnimatedSprite2D = $AnimatedSprite2D
+
 var compressor
 
 var collisionThing
@@ -25,6 +27,18 @@ func _physics_process(delta: float) -> void:
 	
 	moveDir.x = Input.get_axis("move_left", "move_right")
 	moveDir.y = Input.get_axis("move_up", "move_down")
+	
+	if (moveDir.x < 0 && moveDir.y == 0):
+		animated_sprite_2D.animation = "left"
+	
+	if (moveDir.x > 0 && moveDir.y == 0):
+		animated_sprite_2D.animation = "right"
+	
+	if (moveDir.y < 0):
+		animated_sprite_2D.animation = "up"
+	
+	if (moveDir.y > 0):
+		animated_sprite_2D.animation = "down"
 	
 	if moveDir.length() > 0:
 		moveDir = moveDir.normalized()
